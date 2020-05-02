bda27ee5c1f851774b683c7824106e2c0076b402
Pinter Robert <pinterr8833@gmail.com>
2020-05-02 12:46:43 +0200

    update ui basic

--- website/js/app.js
+++ website/js/app.js
@@ -2,6 +2,15 @@
 const api = '&units=metric&appid=24cf83b2850575c3cb8146c500e11ddf';
 const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 let city = '';
+const iconFrontUrl = 'http://openweathermap.org/img/wn/';
+const iconEndUrl = {
+  200: '11d@2x.png', 201: '11d@2x.png', 202: '11d@2x.png', 210: '11d@2x.png', 211: '11d@2x.png', 212: '11d@2x.png', 221: '11d@2x.png', 231: '11d@2x.png', 230: '11d@2x.png', 232: '11d@2x.png',
+  300: '09d@2x.png', 301: '09d@2x.png', 302: '09d@2x.png', 310: '09d@2x.png', 311: '09d@2x.png', 312: '09d@2x.png', 313: '09d@2x.png', 314: '09d@2x.png', 321: '09d@2x.png',
+  500: '10d@2x.png', 501: '10d@2x.png', 502: '10d@2x.png', 503: '10d@2x.png', 504: '10d@2x.png', 511: '13d@2x.png', 520: '09d@2x.png', 521: '09d@2x.png', 522: '09d@2x.png', 531: '09d@2x.png',
+  600: '13d@2x.png', 601: '13d@2x.png', 602: '13d@2x.png', 611: '13d@2x.png', 612: '13d@2x.png', 613: '13d@2x.png', 615: '13d@2x.png', 616: '13d@2x.png', 620: '13d@2x.png', 621: '13d@2x.png', 622: '13d@2x.png',
+  701: '50d@2x.png', 711: '50d@2x.png', 721: '50d@2x.png', 731: '50d@2x.png', 741: '50d@2x.png', 751: '50d@2x.png', 761: '50d@2x.png', 762: '50d@2x.png', 771: '50d@2x.png', 781: '50d@2x.png',
+  800: '01d@2x.png', 801: '02d@2x.png', 802: '03d@2x.png', 803: '04d@2x.png', 804: '04d@2x.png'
+}
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', generate);
 /* Function called by event listener */
@@ -17,7 +26,7 @@ function generate(e){
               temp: data.main,
               weather: data.weather,
               feelings: feeling,
-              date: new Date().toGMTString()
+              date: new Date().toDateString()
             })
   })
 }
@@ -53,3 +62,25 @@ async function postData(url = '', data = {}){
 }
 
 /* Function to GET Project Data */
+async function updateUI(){
+  const res = await fetch('all')
+  try{
+    const allData = await res.json();
+    const iconId = allData[0].weather[0].id;
+    const icon = await fetch(iconFrontUrl+iconEndUrl[iconId])
+    try{
+      document.getElementById('weather__icon').innerHTML = `<img src="${icon.url}" alt="Weather icon" width="200px" height="200px">`;
+    } catch(error){
+      console.log(error);
+    }
+    console.log(allData);
+    document.getElementById('conent__name').innerHTML = allData[0].city;
+    document.getElementById('weather__status').innerHTML = allData[0].weather[0].main;
+    document.getElementById('weather__morestatus').innerHTML = allData[0].weather[0].description;
+    document.getElementById('weather__temp').innerHTML = Math.round(allData[0].temp.temp)+'°C';
+    document.getElementById('content__feelings').innerHTML = allData[0].feelings;
+    document.getElementById('content__date').innerHTML = allData[0].date;
+  } catch(error){
+    console.log(error);
+  }
+}
