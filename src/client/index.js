8d8f3b87f09a1b1353aea4f14fce22c91f902627
Pinter Robert <pinterr8833@gmail.com>
2020-05-25 13:03:54 +0200

    index styling css

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
-import { generate, loadEntry } from './js/app'
+// import { generate, loadEntry } from './js/app'
 
 import './styles/styles.scss';
 
-document.getElementById('generate').addEventListener('click', generate);
-document.getElementById('history__entries__table').addEventListener('click', loadEntry)
+// document.getElementById('generate').addEventListener('click', generate);
+// document.getElementById('history__entries__table').addEventListener('click', loadEntry)
